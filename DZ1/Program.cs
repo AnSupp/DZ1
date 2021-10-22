66d140138e8d75778ee615c8ec21db041cc9c293
anton <anton@LAPTOP-OBTFOHK5>
2021-10-22 10:38:27 +0300

    Добавьте файлы проекта.

---
+++ DZ1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Задание 1"); 
+            WriteLine("Hello World!!!\n");
+            //ReadKey();
+
+            WriteLine("Задание 2");
+            Write("Hello ");
+            Write("world");
+            Write("!!!");
+
+            ReadLine();
+        }
+    }
+}
